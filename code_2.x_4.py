b88ac902b73f7dc69cce12c48e09c2261b4882b6
1099652816@qq.com <bj441296316>
2016-01-29 13:08:10 +0800

    0129

--- code_2.x_4.py
+++ code_2.x_4.py
@@ -1,25 +1,18 @@
 #__author__ = 'colleen'
 # coding=utf-8
-# ����
 import re
-# ���罻��
-import requests
-# ����ϵͳ����
 import os
 
-# ����һ����
+import requests
+
+
 class Spider:
-    #����һ������
     def savePageInfo(self, _url, _position, _regX):
 
-        # Ҫ������ַ
         url = _url
-        # ���ص�ַ
         position = _position
-        # ��ȡ��ҳԴ����
         html = requests.get(url).text
 
-        # ����
         regX = _regX
 
         pic_url = re.findall(regX,html,re.S)
@@ -27,9 +20,8 @@ class Spider:
         i = 0
         for each in pic_url:
 
-            pic = requests.get( each )
-            print  url + each
-            # ����ļ��в����ڣ��򴴽�һ���ļ���
+            pic = requests.get(each)
+            print url + each
             if not os.path.isdir(position):
 
                 os.makedirs(position)
@@ -41,19 +33,14 @@ class Spider:
             i+=1
 
 
-#����������������������������������������ҳ��ȡͼƬ������������������������������������������������
 
 position_end = ''
 
-# Ҫ������ַ
 url = 'http://tieba.baidu.com/p/3590998005' + position_end
 
-# ���ص�ַ
 position = '/Users/edison/Desktop/1/' + position_end
 
-# ����
 regX = '_blank\'><img src=(.*?) t'
 
-#���� url, ����λ��, ��ȡ������
 spider = Spider()
 spider.savePageInfo(url, position, regX)
